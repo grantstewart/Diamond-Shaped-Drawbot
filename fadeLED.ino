2a3644f3cbf11c2020ae933e113977673448b8e3
grantstewart <grant.c.stewart@gmail.com>
2016-01-19 20:52:14 +1100

    Improved fade on the LED

--- fadeLED.ino
+++ fadeLED.ino
@@ -5,7 +5,7 @@ void fadeLED(){
     analogWrite(2, brightness);    
     
     // change the brightness for next time through the loop:
-    brightness = brightness + fadeAmount/10;
+    brightness = brightness + fadeAmount/2;
 
     // reverse the direction of the fading at the ends of the fade:
     if (brightness == 0 || brightness == 255) {
