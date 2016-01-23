700073f10a1a711d8430eb4a2fcc8b9cf5be524a
grantstewart <grant.c.stewart@gmail.com>
2016-01-23 15:36:55 +1100

    New drawing styles  - sensor activated

---
+++ DiamondDrawingDrawbot/DiamondDrawingDrawbot.ino
@@ -0,0 +1,233 @@
+#include <AFMotor.h>
+#include <NewPing.h>
+
+#define TRIGGER_PIN  A5  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     A4  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+///////////////////////////////////LED FADE SETUP///////////////////////////////
+int brightness = 0;    // how bright the LED is
+int fadeAmount = 1;    // how many points to fade the LED by
+unsigned long currentTime;
+unsigned long loopTime;
+
+//////////////LED BLINK
+// Variables will change:
+int ledState = HIGH;             // ledState used to set the LED
+long previousMillis = 0;        // will store last time LED was updated
+
+// the follow variables is a long because the time, measured in miliseconds,
+// will quickly become a bigger number than can be stored in an int.
+long interval = 500;           // interval at which to blink (milliseconds)
+////////////////////////////////////////////////////
+
+
+//////////////////////////////DECLARE THE MOTION SENSOR////////////////////
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+
+
+/////////////////////////////////////////////////////////////////////////////DRAWING SPACE SETUP//////////////////////////////////////////////////////////
+int drawingWidth = 190; // width of the drawing space in CM
+int motorSpeed = 4;  //the speed of the motors  (6-7 is about the max speed with a weight  - 2-3 max without weight)
+
+
+#define TRIGGER_PIN  A4  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     A5  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 300 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+int ledPin = 14;  //define the pin that the LED is on
+
+/////// 1000 steps = 30cm wide diamond
+/////// 1cm = 33.3 steps
+/////// 
+/////// 
+/////// 
+
+int upperLimit = 1;  //the top of the drawing space
+int lowerLimit = drawingWidth*33; // the size of the sides of the diamond (roughly - adjust to suit on install)
+int L = lowerLimit-1; // Left motor string length - at the bottom of the drawing space (-1 because we add 1 in the setup to engage motors)
+int R = lowerLimit-1; // Right Motor string length - within the defined space above (-1 because we add 1 in the setup to engage motors)
+
+///////////////////////////////// DRAWING STYLE VARIABLES - probably get these sorted out before the show ///////////////////////////
+int stepSizeLimit = 30; //this is for the MediumStep function - currently not used
+int lateralLineLimit; // this is the max length of the long lateral lines when going up or down
+int lineGap; // max 'large gap' between dense lines
+int randoChoice;
+int lineGapChoice;
+
+int randoChoiceLimit;
+
+int lateralLineLargeLimit = 1700;
+int lateralLineSmallLimit = 130;
+int percentChanceOfChoosingLargeLineLimit = 10;
+
+
+int lineGaplargeLimit = 150; 
+int lineGapSmallLimit = 50;
+int percentChangeOfChoosingGapLargeLimit = 10;
+
+//////////////////////VERTICAL LINES//////////////
+
+int verticalSizeLineLimit = 400;
+
+// Stepper 200 steps per revolution (or change to 400 for interleave)
+AF_Stepper LM(400, 2),RM(400,1);
+
+////////////////////////////////////////////////////////////////////////////////////
+void setup() {
+  Serial.begin(38400);
+  randomSeed(analogRead(0));// set up Serial library at 9600 bps
+  ///////////////////////////////////////////LED fading variables//////////////
+  // declare pin 2 to be an output:
+  pinMode(2, OUTPUT);
+  currentTime = millis();
+  loopTime = currentTime;
+  delay(2000);
+  ///////////////////////////////////////MOTOR SETUP///////////////////////
+  LM.setSpeed(motorSpeed); //initiate left motor speed
+  RM.setSpeed(motorSpeed); //initiate right motor speed
+  delay(2000);
+  ///////////////////////////////////////ENGAGE Motors//////////////////
+  RM.step(1, BACKWARD, INTERLEAVE); //engage right motor
+  LM.step(1, BACKWARD, INTERLEAVE); //engage left motor
+  delay(4000);
+
+  // Stepper 200 steps per revolution (or change to 400 for interleave)
+  AF_Stepper LM(400, 2),RM(400,1);
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  /*
+void setup() {
+   Serial.begin(38400);
+   randomSeed(analogRead(0));// set up Serial library at 9600 bps
+   delay(2000);
+   LM.setSpeed(motorSpeed); //initiate left motor speed
+   RM.setSpeed(motorSpeed); //initiate right motor speed
+   delay(2000);
+   RM.step(1, BACKWARD, INTERLEAVE); //engage right motor
+   LM.step(1, BACKWARD, INTERLEAVE); //engage left motor
+   pinMode(ledPin, OUTPUT);  
+   delay(2000);  //wait 4 seconds, then start the bot going
+   */
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void loop() {
+
+
+   
+  ////////////////////////////////////////////SENSOR INPUT for HC-SR04//////////////////////////////////////////////////////////////////////////////////////
+  delay(50);                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+  //Serial.print("Ping: ");
+  //Serial.print(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
+  // Serial.println("cm");
+
+      ///////////////////////Do these things when sensor is active or not//////////////////////////
+  while(sonar.ping_cm() <10){  //while the sensor is reading 'not much' set the motor speed to 0 and the LED to fade
+    fadeLED();
+    LM.setSpeed(2); //initiate left motor speed
+    RM.setSpeed(2); //initiate right motor speed
+    DefaultSmallStep();
+    fadeLED();
+  }
+ 
+  while(sonar.ping_cm() >=10){ //while the sensor is reading HIGH, set motors moving and turn on the LED
+    
+    LM.setSpeed(6); //initiate left motor speed
+     RM.setSpeed(6); //initiate right motor speed
+     
+     int choice = int(random(4));
+     if (choice == 0){
+     LeftSideUpLineShapes();
+     
+     } else if (choice ==1){
+     RightSideUpLineShapes();
+     
+     } else if (choice ==2){
+     LeftSideDownLineShapes();
+     
+     } else {
+     RightSideDownLineShapes();
+    
+     }
+     fadeLED();
+  }
+     
+     
+
+  
+  
+/*
+ LM.setSpeed(6); //initiate left motor speed
+     RM.setSpeed(6); //initiate right motor speed
+    LeftSideUpLineShapes();
+    RightSideUpLineShapes();
+    LeftSideDownLineShapes();
+    RightSideDownLineShapes();
+
+  }
+*/
+  //fadeLED();
+
+  ////////////////////////////////////////////////////////////MOVE THE BOT WHILE TESTING BOUNDARIES////////////////////////////////////////////////////////////////////////////////// 
+  /*
+   LeftSideUpLineShapes();
+   RightSideUpLineShapes();
+   LeftSideDownLineShapes();
+   RightSideDownLineShapes();
+   */
+
+  // DefaultSmallStep();
+  //verticalLinesDown(); 
+  //testBoundaries();
+  //testBox();
+
+
+
+
+
+  /*
+  ////////////////////////////////////////////SENSOR INPUT//////////////////////////////////////////////////////////////////////////////////////
+   delay(50);                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+   Serial.print("Ping: ");
+   Serial.print(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
+   Serial.println("cm");
+   while(sonar.ping_cm() <10){  //while the sensor is reading 'not much' set the motor speed to 0 and the LED to off.
+   digitalWrite(ledPin, LOW); 
+   LM.setSpeed(0);
+   RM.setSpeed(0);
+   
+   }
+   while(sonar.ping_cm() >10){ //while the sensor is reading HIGH, set motors moving and turn on the LED
+   digitalWrite(ledPin, HIGH); 
+   LM.setSpeed(4);
+   RM.setSpeed(4);
+   }
+   
+   */
+
+
+
+  /////////////////////////////////////////////////////CHOOSE DRAWING STYLE BASED ON LINE LENGTH AND LINE GAP VARIABLES //////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+  ////////////////////////////////////////////////////////////MOVE THE BOT WHILE TESTING BOUNDARIES////////////////////////////////////////////////////////////////////////////////// 
+  /*
+  LeftSideUpLineShapes();
+   RightSideDownLineShapes();
+   LeftSideDownLineShapes();
+   RightSideUpLineShapes();
+   */
+  //testBoundaries();
+
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //testBox();
+}
+
+
+
+
+
+
---
+++ DiamondDrawingDrawbot/Left_SideUp_LineShapes.ino
@@ -0,0 +1,63 @@
+void LeftSideUpLineShapes(){
+  //////////////SETS UP THE STYLE OF DRAWING////////////
+  randoChoice = int(random(100));  // this defines how long the long back and forth lines are
+  if (randoChoice<percentChanceOfChoosingLargeLineLimit){
+    lateralLineLimit = int(random(lateralLineLargeLimit));
+  }
+  else{
+    lateralLineLimit = random(lateralLineSmallLimit);
+  }
+  lineGapChoice = int(random(100));  // this defines the amound of gap separating the long back and forth lines
+  if (lineGapChoice<percentChangeOfChoosingGapLargeLimit){
+    lineGap = int(random(lineGaplargeLimit));
+
+    //////////////SETS UP THE STYLE OF DRAWING////////////
+    randoChoice = int(random(100));  // this defines how long the long back and forth lines are
+    if (randoChoice<3){
+      lateralLineLimit = int(random(500));
+    }
+    else{
+      lateralLineLimit = random(30);
+    }
+    lineGapChoice = int(random(100));  // this defines the amound of gap separating the long back and forth lines
+    if (lineGapChoice<2){
+      lineGap = int(random(50));
+
+    } 
+    else {
+      lineGap = int(random(50));
+    }
+    int zizzo = int(random(lineGap)); //choose random 0 1 2 3 
+    int zizzo2 = int(random(lateralLineLimit)); //choose random 0 1 2 3 
+    int numLines = int(random(13));
+
+    for(int i=0;i<numLines;i++){
+
+
+      testBoundaries();
+      LM.step(zizzo, BACKWARD, INTERLEAVE); 
+      L-=zizzo;
+      digitalWrite(2, LOW);
+      testBoundaries();
+      RM.step(zizzo2,BACKWARD,INTERLEAVE);
+      R+=zizzo2;
+      digitalWrite(2, HIGH);
+      testBoundaries();
+      LM.step(zizzo,BACKWARD, INTERLEAVE);
+      L-=zizzo;
+      digitalWrite(2, LOW);
+      testBoundaries();
+      RM.step(zizzo2,FORWARD,INTERLEAVE);
+      R-=zizzo2;
+      digitalWrite(2, HIGH);
+      testBoundaries();
+
+    }
+
+
+
+
+  }
+}
+
+
