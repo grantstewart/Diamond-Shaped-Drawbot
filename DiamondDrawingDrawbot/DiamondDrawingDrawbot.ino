a3b1a3cf7f2cf6910fb811c048710c923f198af9
grantstewart <grant.c.stewart@gmail.com>
2016-01-28 21:25:28 +1100

    Removed a bunch of redundant code. changed drawing area size

--- DiamondDrawingDrawbot/DiamondDrawingDrawbot.ino
+++ DiamondDrawingDrawbot/DiamondDrawingDrawbot.ino
@@ -27,7 +27,7 @@ NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and
 
 
 /////////////////////////////////////////////////////////////////////////////DRAWING SPACE SETUP//////////////////////////////////////////////////////////
-int drawingWidth = 190; // width of the drawing space in CM
+int drawingWidth = 250; // width of the drawing space in CM
 int motorSpeed = 5;  //the speed of the motors  (6-7 is about the max speed with a weight  - 2-3 max without weight)
 
 
@@ -91,23 +91,7 @@ void setup() {
   // Stepper 200 steps per revolution (or change to 400 for interleave)
   AF_Stepper LM(400, 2),RM(400,1);
 
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  /*
-void setup() {
-   Serial.begin(38400);
-   randomSeed(analogRead(0));// set up Serial library at 9600 bps
-   delay(2000);
-   LM.setSpeed(motorSpeed); //initiate left motor speed
-   RM.setSpeed(motorSpeed); //initiate right motor speed
-   delay(2000);
-   RM.step(1, BACKWARD, INTERLEAVE); //engage right motor
-   LM.step(1, BACKWARD, INTERLEAVE); //engage left motor
-   pinMode(ledPin, OUTPUT);  
-   delay(2000);  //wait 4 seconds, then start the bot going
-   */
 }
-
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void loop() {
 
@@ -118,14 +102,14 @@ void loop() {
   //Serial.print("Ping: ");
   //Serial.print(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
   // Serial.println("cm");
-
+/*
   ///////////////////////Do these things when sensor is active or not//////////////////////////
   while(sonar.ping_cm() <10){  //while the sensor is reading 'not much' set the motor speed to 0 and the LED to fade
     fadeLED();
     LM.setSpeed(3); //initiate left motor speed
     RM.setSpeed(3); //initiate right motor speed
     DefaultSmallStep();
-    fadeLED();
+    //fadeLED();
   }
 
   while(sonar.ping_cm() >=10){ //while the sensor is reading HIGH, set motors moving and turn on the LED
@@ -150,24 +134,10 @@ void loop() {
       RightSideDownLineShapes();
 
     }
-    // fadeLED();
+    //fadeLED();
   }
 
-
-
-
-
-  /*
- LM.setSpeed(6); //initiate left motor speed
-   RM.setSpeed(6); //initiate right motor speed
-   LeftSideUpLineShapes();
-   RightSideUpLineShapes();
-   LeftSideDownLineShapes();
-   RightSideDownLineShapes();
-   
-   }
-   */
-  //fadeLED();
+*/
 
   ////////////////////////////////////////////////////////////MOVE THE BOT WHILE TESTING BOUNDARIES////////////////////////////////////////////////////////////////////////////////// 
   /*
@@ -180,49 +150,7 @@ void loop() {
   // DefaultSmallStep();
   //verticalLinesDown(); 
   //testBoundaries();
-  //testBox();
-
-
-
-
-
-  /*
-  ////////////////////////////////////////////SENSOR INPUT//////////////////////////////////////////////////////////////////////////////////////
-   delay(50);                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
-   Serial.print("Ping: ");
-   Serial.print(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
-   Serial.println("cm");
-   while(sonar.ping_cm() <10){  //while the sensor is reading 'not much' set the motor speed to 0 and the LED to off.
-   digitalWrite(ledPin, LOW); 
-   LM.setSpeed(0);
-   RM.setSpeed(0);
-   
-   }
-   while(sonar.ping_cm() >10){ //while the sensor is reading HIGH, set motors moving and turn on the LED
-   digitalWrite(ledPin, HIGH); 
-   LM.setSpeed(4);
-   RM.setSpeed(4);
-   }
-   
-   */
-
-
-
-  /////////////////////////////////////////////////////CHOOSE DRAWING STYLE BASED ON LINE LENGTH AND LINE GAP VARIABLES //////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-  ////////////////////////////////////////////////////////////MOVE THE BOT WHILE TESTING BOUNDARIES////////////////////////////////////////////////////////////////////////////////// 
-  /*
-  LeftSideUpLineShapes();
-   RightSideDownLineShapes();
-   LeftSideDownLineShapes();
-   RightSideUpLineShapes();
-   */
-  //testBoundaries();
-
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //testBox();
+  testBox();
 }
 
 
